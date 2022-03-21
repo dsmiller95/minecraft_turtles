ea56db93064fe77098f35a08618b9e108df3e3cc
Dan Miller <dsmiller95@gmail.com>
2022-03-20 21:13:00 -0500

    update building tools to attempt to refil a target slot if possible

--- script/programs/lib/buildingTools.lua
+++ script/programs/lib/buildingTools.lua
@@ -3,10 +3,18 @@ local MAX_TURTLE_SLOT = 16;
 
 
 function PlaceBlockFromSlotSafeDown(slotNumber)
+    turtle.select(slotNumber);
     if turtle.getItemCount(slotNumber) <= 1 then
+        for i = 1, 16 do
+            if i ~= slotNumber and turtle.compareTo(i) then
+                turtle.select(i);
+                turtle.transferTo(slotNumber);
+                turtle.select(slotNumber);
+                break;
+            end
+        end
         error("ran out of items in slot " .. slotNumber)
     end
-    turtle.select(slotNumber);
     return turtle.placeDown();
 end
 
