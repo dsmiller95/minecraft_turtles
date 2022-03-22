7d502e11b58af6ebadff3e9f79205ee5df63bfd1
Dan Miller <dsmiller95@gmail.com>
2022-03-22 16:34:44 -0500

    add script to track and cache position, proxying all turtle movement calls

--- script/programListing.lua
+++ script/programListing.lua
@@ -1,6 +1,6 @@
 
 return {
     allPrograms = {"spiralTurtle", "straightTurtle", "mineToFloor", "patternPrint"},
-    allLibs = {"fuelingTools", "buildingTools", "inventoryTools"},
-    version = "1.0.8"
+    allLibs = {"fuelingTools", "buildingTools", "inventoryTools", "positionProvider"},
+    version = "1.0.9"
 };
---
+++ script/programs/lib/positionProvider.lua
@@ -0,0 +1,100 @@
+
+
+local currentPosition = vector.new(gps.locate());
+print("found position: " .. currentPosition);
+
+-- range from 0 to 3
+local currentDirection = nil;
+
+local directionToDiff = {
+    vector.new( 1, 0, 0),
+    vector.new( 0, 0, 1),
+    vector.new(-1, 0, 0),
+    vector.new( 0, 0,-1),
+}
+
+local function MoveUp()
+    local didMove, error = turtle.up();
+    if didMove then
+        currentPosition.y = currentPosition.y + 1;
+        return true;
+    end
+    return didMove, error;
+end
+
+local function MoveDown()
+    local didMove, error = turtle.down();
+    if didMove then
+        currentPosition.y = currentPosition.y + 1;
+    end
+    return didMove, error;
+end
+
+local function DeriveDirectionAfterMove()
+    -- we don't know what direction we're facing yet. take a measurement and figure it out
+    local nextPosition = vector.new(gps.locate());
+    local diff = nextPosition - currentPosition;
+    for i = 0, 3 do
+        local directionVect = directionToDiff[i + 1];
+        if directionVect == diff then
+            print("found direction vector: " .. directionVect);
+            return i;
+        end
+    end
+end
+
+local function MoveForward()
+    local didMove, error = turtle.forward();
+    if didMove then
+        if not currentDirection then
+            currentDirection = DeriveDirectionAfterMove();
+        end
+        currentPosition = currentPosition:add(directionToDiff[currentDirection + 1]);
+        return true;
+    end
+    return didMove, error;
+end
+
+local function MoveBack()
+    local didMove, error = turtle.back();
+    if didMove then
+        if not currentDirection then
+            currentDirection = (DeriveDirectionAfterMove() + 2) % 4;
+        end
+        currentPosition = currentPosition:add(directionToDiff[currentDirection + 1]);
+        return true;
+    end
+    return didMove, error;
+end
+
+local function TurnRight()
+    local didMove, error = turtle.turnRight();
+    if didMove and currentDirection then
+        currentDirection = (currentDirection + 1) % 4;
+        return true;
+    end
+    return didMove, error;
+end
+local function TurnLeft()
+    local didMove, error = turtle.turnLeft();
+    if didMove and currentDirection then
+        currentDirection = (currentDirection + 3) % 4;
+        return true;
+    end
+    return didMove, error;
+end
+
+local function Position()
+    return currentPosition
+end
+
+
+return {
+    up=MoveUp,
+    down=MoveDown,
+    forward=MoveForward,
+    back=MoveBack,
+    turnRight=TurnRight,
+    turnLeft=TurnLeft,
+    Position=Position,
+}
