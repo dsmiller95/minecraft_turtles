492f9e52ec8e24db196c990a9b4199fa7e8c1403
Dan Miller <dsmiller95@gmail.com>
2022-03-26 15:04:22 -0500

    fix invoke bug

--- script/programListing.lua
+++ script/programListing.lua
@@ -9,5 +9,5 @@ return {
         "lib/rednetHelpers", "lib/generatorTools",
         "jobs/deployServiceGrid", "jobs/dummyJob"
     },
-    version = "1.0.78"
+    version = "1.0.79"
 };
--- script/programs/runJob.lua
+++ script/programs/runJob.lua
@@ -3,7 +3,7 @@ local jobLib = require("lib.jobClientLibrary");
 local jobCommand = table.concat(arg, " ");
 
 jobLib.InitLog();
-local success = jobLib.RunJob(jobCommand);
+local success = jobLib.RunJob({job = jobCommand});
 jobLib.CloseLog()
 
 print("job success: " .. tostring(success));
