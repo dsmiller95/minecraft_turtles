4b33d1abde97037bf848e1d2d7069ade63ca8021
Dan Miller <dsmiller95@gmail.com>
2022-03-26 20:22:31 -0500

    write chunk data to disk

--- script/programs/chunkStatusServer.lua
+++ script/programs/chunkStatusServer.lua
@@ -3,8 +3,8 @@ local constants = require("lib.turtleMeshConstants");
 
 local allChunks = {};
 
-local function ToChunkIndex(x, z)
-    return tostring(x) .. "," .. tostring(z);
+local function ToChunkFileName(x, z)
+    return tostring(x) .. "," .. tostring(z) .. ".chunk";
 end
 
 -- Meta class
@@ -13,13 +13,41 @@ Chunk = {status=constants.CHUNK_STATUS.WILDERNESS, x=nil, z = nil}
 -- Derived class method new
 
 function Chunk:new(x, z)
-   local o = {};
-   setmetatable(o, self);
-   self.__index = self;
-   o.status = constants.CHUNK_STATUS.WILDERNESS;
-   o.x = x;
-   o.z = z;
-   return o;
+    local o = {};
+    setmetatable(o, self);
+    self.__index = self;
+    o.status = constants.CHUNK_STATUS.WILDERNESS;
+    o.x = x;
+    o.z = z;
+    return o;
+ end
+ function Chunk:deserialize(obj)
+    local o = obj;
+    setmetatable(o, self);
+    self.__index = self;
+    return o;
+ end
+
+function Chunk:serialize()
+    return self;
+end
+
+local function ReadChunkData(x, z)
+    local file = fs.open(ToChunkFileName(x, z), "r");
+    if not file then
+        return nil;
+    end
+    local chunkData = textutils.unserialize(file.readAll());
+    file.close();
+    return Chunk:deserialize(chunkData);
+end
+
+local function WriteChunkData(x, z, chunk)
+    local data =  textutils.serialize(chunk:serialize());
+
+    local file = fs.open(ToChunkFileName(x, z), "w");
+    file.write(data);
+    file.close();
 end
 
 
@@ -45,8 +73,7 @@ local function GetChunkData(senderId, message)
     local s, e, xStr, zStr = string.find(message, "status %((-?%d+), (-?%d+)%)");
     local x = tonumber(xStr);
     local z = tonumber(zStr);
-    local index = ToChunkIndex(x, z);
-    local chunk = allChunks[index];
+    local chunk = ReadChunkData(x, z);
     if not chunk then
         print("sending resp " .. tostring(senderId) .. " : " .. constants.CHUNK_STATUS.WILDERNESS);
         rednet.send(senderId, constants.CHUNK_STATUS.WILDERNESS,  "CHUNKRESP");
@@ -59,13 +86,12 @@ local function UpdateChunkStatus(senderId, message)
     local s, e, xStr, zStr, newStatus = string.find(message, "update %((-?%d+), (-?%d+)%) : {(.*)}");
     local x = tonumber(xStr);
     local z = tonumber(zStr);
-    local index = ToChunkIndex(x, z);
-    local chunk = allChunks[index];
+    local chunk = ReadChunkData(x, z)
     if not chunk then
         chunk = Chunk:new(x, z);
     end
     chunk.status = newStatus;
-    allChunks[index] = chunk;
+    WriteChunkData(x, z, chunk);
     print("sending resp " .. tostring(senderId) .. " : " .. chunk.status);
     rednet.send(senderId, chunk.status, "CHUNKRESP");
 end
